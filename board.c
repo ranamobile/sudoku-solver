5ecccdaad361b36698f488acd3a8be03fb533ba9
Ariana Hong <ariana@blackcape.io>
2020-06-21 16:54:36 -0400

    solve up to medium problems (level 6). add example files.

--- board.c
+++ board.c
@@ -10,11 +10,22 @@ typedef struct Board
 
 int * get_potential_values(Board * board, int row, int col)
 {
-    int * potential = (int *) malloc(sizeof(int) * 9);
-    for (int option = 0; option < 9; option++)
+    int * potential = (int *) malloc(sizeof(int) * 10);
+    int value = board->spaces[row][col];
+    for (int option = 0; option < 10; option++)
     {
-        potential[option] = 1;
+        if (value == 0)
+        {
+            potential[option] = 0;
+        }
+        else
+        {
+            potential[option] = 1;
+        }
+
     }
+
+    potential[0] = 1;
     for (int trow = 0; trow < 9; trow++)
     {
         int value = board->spaces[trow][col];
@@ -23,15 +34,105 @@ int * get_potential_values(Board * board, int row, int col)
     for (int tcol = 0; tcol < 9; tcol++)
     {
         int value = board->spaces[row][tcol];
-        potential[value] = 0;
+        potential[value] = 1;
+    }
+
+    int rfactor = row / 3;
+    int cfactor = col / 3;
+    rfactor = rfactor * 3;
+    cfactor = cfactor * 3;
+    for (int trow = rfactor; trow < rfactor + 3; trow++)
+    {
+        for (int tcol = cfactor; tcol < cfactor + 3; tcol++)
+        {
+            int value = board->spaces[trow][tcol];
+            potential[value] = 1;
+        }
     }
 
     return potential;
 }
 
+
+int get_number_of_possibilities(int * possibilities)
+{
+    int count = 0;
+    for (int option = 1; option < 10; option++)
+    {
+        if (possibilities[option] == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+int get_next_option(int * possibilities, int index)
+{
+    int count = 0;
+    for (int option = index; option < 10; option++)
+    {
+        if (possibilities[option] == 0)
+        {
+            return option;
+        }
+    }
+    return -1;
+}
+
+
+int fill_known_values(Board * board)
+{
+    int * possibilities = NULL;
+    int fills = 0;
+    for (int row = 0; row < 9; row++)
+    {
+        for (int col = 0; col < 9; col++)
+        {
+            possibilities = get_potential_values(board, row, col);
+            if (get_number_of_possibilities(possibilities) == 1)
+            {
+                int option = get_next_option(possibilities, 0);
+                board->spaces[row][col] = option;
+                fills++;
+                printf("Filling %d, %d: %d\n", row, col, option);
+            }
+            if (possibilities)
+            {
+                free(possibilities);
+                possibilities = NULL;
+            }
+        }
+    }
+    return fills;
+}
+
+
 Board * load_board(char * filepath)
 {
+    FILE * fp = fopen(filepath, "r");
+    ssize_t read;
+    size_t len = 0;
 
+    Board * board = (Board *) malloc(sizeof(Board));
+    int value;
+
+    if (fp == NULL)
+    {
+        return NULL;
+    }
+
+    for (int row = 0; row < 9; row++) {
+        for (int col = 0; col < 9; col++)
+        {
+            fscanf(fp, "%i ", &value);
+            board->spaces[row][col] = value;
+        }
+    }
+
+    fclose(fp);
+    return board;
 }
 
 
@@ -50,7 +151,7 @@ void print_board(Board * board)
             {
                 printf(" |");
             }
-            printf(" %d ", board->spaces[row][col] + 1);
+            printf(" %d ", board->spaces[row][col]);
         }
         printf("\n");
     }
@@ -58,26 +159,24 @@ void print_board(Board * board)
 }
 
 
-int main()
+int main(int argc, char * argv[])
 {
-    Board * board = (Board *) malloc(sizeof(Board));
-    for (int row = 0; row < 9; row++)
+    Board * board;
+    if (argc != 2)
     {
-        for (int col = 0; col < 9; col++)
-        {
-            board->spaces[row][col] = 0;
-        }
+        exit(EXIT_FAILURE);
     }
+
+    printf("Filepath: %s\n", argv[1]);
+    board = load_board(argv[1]);
 
     print_board(board);
 
-    int * possibilities = get_potential_values(board, 0, 0);
-    for (int option = 0; option < 9; option++)
+    int fills;
+    while ((fills = fill_known_values(board)) > 0)
     {
-        if (possibilities[option] > 0)
-        {
-            printf("%d ", option + 1);
-        }
+        print_board(board);
     }
-    printf("\n");
+
+    free(board);
 }
